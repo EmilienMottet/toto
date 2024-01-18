53fbf85188f4eda70c7910595d6baae9e9b10015
EmilienMottet <emilien.mottet@grenoble-inp.org>
2024-01-18 13:58:00 +0100

    fix bad js

--- static/01-ch00Z3-uR-PASS-script.js
+++ static/01-ch00Z3-uR-PASS-script.js
@@ -40,18 +40,18 @@ for(var i = 0; i < buttons.length; i++) {
 }
 
 
-//
-// ---Retro Submit Button---
-//
-var pButton = document.querySelector('.loader-button');
+// //
+// // ---Retro Submit Button---
+// //
+// var pButton = document.querySelector('.loader-button');
 
-// Click
-pButton.addEventListener('mousedown', function() {
-  this.classList.add('btn-active');
-});
-pButton.addEventListener('mouseup', function() {
-  this.classList.remove('btn-active');
-});
+// // Click
+// pButton.addEventListener('mousedown', function() {
+//   this.classList.add('btn-active');
+// });
+// pButton.addEventListener('mouseup', function() {
+//   this.classList.remove('btn-active');
+// });
 
 
 // Classie Helper Functions
